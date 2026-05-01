934cbb351f01f419c1fbe3bac494395b8470566b
JasonWu-ux <jasonwu544@gmail.com>
2026-05-01 01:21:34 +0000

    finished gamepiece class

--- src/main/java/ProblemSet.java
+++ src/main/java/ProblemSet.java
@@ -4,6 +4,62 @@ public class ProblemSet {
 
 	}
 
+
+	public class GamePiece {
+		private String label;
+		private String color;
+		private int pointValue;
+
+	public GamePiece(String label, String color, int pointValue) {
+		this.label = label;
+		this.color = color;
+		this.pointValue = pointValue;
+	}
+	//Getters
+	public String getLabel() {
+		return label;
+	}
+
+	public String getColor() {
+		return color;
+	}
+	
+	public int getPointValue() {
+		return pointValue;
+	}
+
+	public String toString() {
+		return color + " " + label;
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj) 
+			return true;
+		if(obj == null || getClass() != obj.getClass())
+			return false;
+		GamePiece other == (GamePiece ) obj;
+		return this.pointValue == other.pointValue
+				&& this.label.equals(other.label) && this.color.equals(other.color);
+	}
+}
+
+
+
+class label 
+	private String Pawn;
+	private String Boot;
+	private String Car;
+	private String Mouse;
+
+
+
+
+	colors of piece  	piece pawn, boot,car, mouse
+	Red
+	Green
+	Purple
+	Yellow
+
+
 	
 
-}
