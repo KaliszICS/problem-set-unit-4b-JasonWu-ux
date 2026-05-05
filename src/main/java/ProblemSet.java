55f85c64b50b737f514fb7847899aa27b99ec605
JasonWu-ux <jasonwu544@gmail.com>
2026-05-05 01:38:25 +0000

    some more progess on gameboard class

--- src/main/java/ProblemSet.java
+++ src/main/java/ProblemSet.java
@@ -4,7 +4,6 @@ public class ProblemSet {
 
 	}
 
-
 	public class GamePiece {
 		private String label;
 		private String color;
@@ -15,6 +14,7 @@ public class ProblemSet {
 		this.color = color;
 		this.pointValue = pointValue;
 	}
+
 	//Getters
 	public String getLabel() {
 		return label;
@@ -37,29 +37,57 @@ public class ProblemSet {
 			return true;
 		if(obj == null || getClass() != obj.getClass())
 			return false;
-		GamePiece other == (GamePiece ) obj;
+		GamePiece other = (GamePiece ) obj;
 		return this.pointValue == other.pointValue
 				&& this.label.equals(other.label) && this.color.equals(other.color);
 	}
 }
+	public class GameBoard {
+		private String[][] tiles;
+		private GamePiece[][] pieces;
 
+		public GameBoard(String[][] layout) {
+			tiles = layout;
+			pieces = new GamePiece[layout.length][layout[0].length];
+		}
+		public GameBoard() {
+    	tiles = new String[5][5];
+   	 	pieces = new GamePiece[5][5];
 
+    	for (int row = 0; row < 5; row++) {
+        for (int col = 0; col < 5; col++) {
+            
+            int tileNumber = (row * 5) + col + 1;
 
-class label 
-	private String Pawn;
-	private String Boot;
-	private String Car;
-	private String Mouse;
+            if (tileNumber == 1) {
+                tiles[row][col] = "Start";
+            } else if (tileNumber == 25) {
+                tiles[row][col] = "End";
+            } else if (tileNumber % 6 == 0) {
+                tiles[row][col] = "Penalty";
+            } else if (tileNumber % 4 == 0) {
+                tiles[row][col] = "Bonus";
+            } else {
+                tiles[row][col] = "Empty";
+            }
+        }
+    }
+}
 
+		public int getRows() {
+		return tiles.length;
+	}
+		public int getCols() {
+		return tiles[0].length;
+	}
+	public String getTile(int row, int col) {
+		if (row < 0 || row >= getRows() || col < 0 || col >= getCols()) {
+			throw new ArrayIndexOutOfBoundsException("Out of Bounds");
 
-
-
-	colors of piece  	piece pawn, boot,car, mouse
-	Red
-	Green
-	Purple
-	Yellow
-
-
+		} 
+		return tiles[row][col];
+	}
+}
+}
 	
 
